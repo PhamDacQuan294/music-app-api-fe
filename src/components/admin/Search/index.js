10a32d6d3cf2e73ed78090770ec565d9068b3570
quan <quan93909@gmail.com>
2025-09-25 16:24:31 +0700

    Tìm kiếm hiển thị suggest (gợi ý)

--- src/components/admin/Search/index.js
+++ src/components/admin/Search/index.js
@@ -1,11 +1,28 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Input } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { searchService } from "../../../services/admin/searchService";
+import "./Search.scss"
 
 function Search({ onSearchResult, type, placeholder }) {
   const [keyword, setKeyword] = useState("");
+  const [suggests, setSuggests] = useState([]);
   const navigate = useNavigate();
+
+  // Khi gõ từ khóa thì gọi API suggest
+  useEffect(() => {
+    const delayDebounce = setTimeout(async () => {
+      if (keyword.trim()) {
+        const data = await searchService(`${type}`, keyword);
+        setSuggests(data?.[type]);
+      } else {
+        setSuggests([]);
+      }
+    }, 300);
+
+    return () => clearTimeout(delayDebounce);
+  }, [keyword]);
 
   const handleSearch = async () => {
     if (keyword) {
@@ -14,6 +31,8 @@ function Search({ onSearchResult, type, placeholder }) {
         onSearchResult(data?.[type]);
       }
       navigate(`/admin/${type}?search-${type}=${encodeURIComponent(keyword)}`);
+
+      setSuggests([]);
     } else {
       navigate(`/admin/${type}`)
       const data = await searchService(`${type}`, keyword);
@@ -34,6 +53,34 @@ function Search({ onSearchResult, type, placeholder }) {
           size="middle"
           placeholder={placeholder}
         />
+        {suggests.length > 0 &&
+          <div className="search__suggest">
+            <div className="search__suggest-list">
+              {suggests.map((item) => (
+                <div
+                  key={item._id}
+                  className="search__suggest-item"
+                  onClick={() => {
+                    setKeyword(item.title);      // set lại input value
+                    onSearchResult([item]);      // click gợi ý nó trả về bái hát đó
+                    setSuggests([]);             // clear gợi ý
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  <div className="search__suggest-image">
+                    <img src={item.avatar} alt={item.title} />
+                  </div>
+                  <div className="search__suggest-info">
+                    <div className="search__suggest-title">{item.title}</div>
+                    <div className="search__suggest-singer">
+                      {item.infoSinger?.fullName}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        }
       </div>
     </>
   )
