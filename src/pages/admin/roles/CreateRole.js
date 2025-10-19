4842f45304576ac5f4c07570221cc123fd9cce05
quan <quan93909@gmail.com>
2025-10-19 19:50:05 +0700

    Làm chức năng tạo nhóm quyền

--- src/pages/admin/roles/CreateRole.js
+++ src/pages/admin/roles/CreateRole.js
@@ -1,7 +1,11 @@
-import { Button, Form } from "antd";
+import { Button, Form, message } from "antd";
 import RoleFormFields from "./RoleFormFields";
+import { createRolePost } from "../../../services/admin/rolesService";
 
 function RoleCreateForm() {
+  const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
+
   const rules = [
     {
       required: true,
@@ -9,13 +13,54 @@ function RoleCreateForm() {
     },
   ];
 
+  const handleSubmit = async (values) => {
+    try {
+      const response = await createRolePost(values);
+
+      if (response.code === 200) {
+        form.resetFields();
+        setTimeout(() => {
+          messageApi.open({
+            type: "success",
+            content: "Tạo nhóm quyền mới thành công",
+            duration: 5,
+          });
+        }, 0);
+      } else if (response.code === 400) {
+        messageApi.open({
+          type: "error",
+          content: response.data.message,
+          duration: 5,
+        });
+      } else {
+        messageApi.open({
+          type: "error",
+          content: "Tạo nhóm quyền mới không thành công",
+          duration: 5,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+
+      messageApi.open({
+        type: "error",
+        content: "Có lỗi kết nối server!",
+        duration: 5,
+      });
+    }
+  }
+
   return (
     <>
-      <Form layout="vertical" name="create-role" initialValues={{ status: true }}>
+      {contextHolder}
+
+       <h2>Thêm nhóm quyền</h2>
+
+      <Form layout="vertical" name="create-role" form={form} onFinish={handleSubmit}>
         <RoleFormFields
           rules={rules}
         />
- 
+
         <Form.Item className="form-submit">
           <Button type="primary" htmlType="submit">
             Tạo mới
--- src/services/admin/rolesService.js
+++ src/services/admin/rolesService.js
@@ -1,9 +1,14 @@
 import { API_PREFIX, PREFIX_ADMIN } from "../../components/admin/Contants";
-import { get } from "../../utils/request";
+import { get, post } from "../../utils/request";
 
 export const getListRoles = async () => {
   let url = `${API_PREFIX}/${PREFIX_ADMIN}/roles/permissions`;
 
   const result = await get(url);
+  return result;
+}
+
+export const createRolePost = async (formData) => {
+  const result = await post(`${API_PREFIX}/${PREFIX_ADMIN}/roles/create`, formData);
   return result;
 }
